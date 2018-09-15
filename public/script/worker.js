fc4a029532e24d9fc0b67a8e8a5fb85b92ba4c61
Mattias Lidbeck <teddykladdkak@gmail.com>
2018-09-15 05:14:32 +0200

    rensar

--- public/script/worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("push", e => {
-  const data = e.data.json();
-  console.log("Push Recieved...");
-  self.registration.showNotification(data.title, {
-    body: data.text,
-    icon: data.icon
-  });
-});
